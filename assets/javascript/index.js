8e8b7597c4caba11b4427eeb7ad7c9791e59c207
Frank87 <franklinsegura@Franklins-MacBook-Air.local>
2017-07-12 09:37:10 -0400

    new changes

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -39,6 +39,39 @@ var TxtType = function(el, toRotate, period) {
         }, delta);
     };
 
+    $('#myCarousel').carousel({
+        interval:   2000
+    });
+    
+    var clickEvent = false;
+    $('#myCarousel').on('click', '.nav a', function() {
+            clickEvent = true;
+            $('.nav li').removeClass('active');
+            $(this).parent().addClass('active');        
+    }).on('slid.bs.carousel', function(e) {
+        if(!clickEvent) {
+            var count = $('.nav').children().length -1;
+            var current = $('.nav li.active');
+            current.removeClass('active').next().addClass('active');
+            var id = parseInt(current.data('slide-to'));
+            if(count == id) {
+                $('.nav li').first().addClass('active');    
+            }
+        }
+        clickEvent = false;
+    });
+
+
+  //scroll effect for anchor id's#
+    $('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 900);
+    return false;
+});
+//end of scroll effect
+
+
 
     window.onload = function() {
         var elements = document.getElementsByClassName('typewrite');
@@ -55,4 +88,4 @@ var TxtType = function(el, toRotate, period) {
         css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
         document.body.appendChild(css);
 
-}
+    };
